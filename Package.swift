735b99a178249a03c3df1ab5cfda7c1d299d85b8
Jader Feijo <jader.feijo@gmail.com>
2019-10-23 12:46:23 +0100

    Fix Swift Package definition & update package swift version to 5.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,22 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
-    name: "LoremSwiftum"
+    name: "LoremSwiftum",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "LoremSwiftum",
+            targets: [
+                "LoremSwiftum"
+            ]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "LoremSwiftum")
+    ]
 )
